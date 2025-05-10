eb944689d1895f23ecbfd223b93b10d35fce406a
Gábor Horváth-Ulenius <gabor.horvath-ulenius@tietoevry.com>
2025-05-10 18:18:48 +0300

    fix flashy theme popup background

--- src/components/VideoScroller.tsx
+++ src/components/VideoScroller.tsx
@@ -5,13 +5,10 @@ import ScrollyVideo from "scrolly-video/dist/ScrollyVideo.esm.jsx";
 export const VideoScroller = () => {
   const { selectedTheme } = useThemeToggle();
 
-  return (
-    <ScrollyVideo
-      src={
-        selectedTheme === "dark"
-          ? "/parallaxMui/gemini-jungle-dark.mp4"
-          : "/parallaxMui/gemini-jungle-light.mp4"
-      }
-    />
-  );
+  const src =
+    selectedTheme === "dark"
+      ? "/parallaxMui/gemini-jungle-dark.mp4"
+      : "/parallaxMui/gemini-jungle-light.mp4";
+
+  return <ScrollyVideo src={src} />;
 };
--- src/hooks/useThemeToggle.ts
+++ src/hooks/useThemeToggle.ts
@@ -1,11 +1,32 @@
-import { atomWithStorage } from "jotai/utils";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
 import { useAtom } from "jotai";
 
 export type Theme = "light" | "dark";
-const themeAtom = atomWithStorage<Theme>("theme", "light");
+
+const getInitialFallbackTheme = (): Theme => {
+  if (typeof window !== "undefined") {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "dark";
+    }
+  }
+  return "light";
+};
+
+const localStorageJSON = createJSONStorage<Theme>(() => localStorage);
+
+const themeAtom = atomWithStorage<Theme>(
+  "theme",
+  getInitialFallbackTheme(),
+  localStorageJSON,
+  { getOnInit: true },
+);
 
 export const useThemeToggle = () => {
   const [selectedTheme, setSelectedTheme] = useAtom(themeAtom);
+
   return {
     selectedTheme,
     toggleTheme: () =>
